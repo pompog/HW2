f1f7055ad37d9d54f4519e9632acb2a0aefe0cca
pompog <puttipongc0204@gmail.com>
2024-09-23 22:03:36 +0700

    first commit

---
+++ express.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname,'public','index.html'));
+});
+
+app.listen(3000, function () {
+  console.log('Routed app listening on port 3000!');
+});
